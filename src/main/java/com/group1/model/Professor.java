720c25514bc7f47664d0f587399c26f6c967ec83
AbhijithSriram <abhijithsriramv28@gmail.com>
2025-10-19 09:20:16 +0530

    First Commit

---
+++ src/main/java/com/group1/model/Professor.java
@@ -0,0 +1,47 @@
+package com.group1.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PROFESSOR")
+public class Professor {
+
+    @Id
+    private String employee_id;
+
+    private String name;
+
+    @Column(unique = true) // Enforce unique constraint
+    private String email;
+
+    private String password_hash;
+
+    // --- Getters and Setters ---
+    public String getEmployee_id() {
+        return employee_id;
+    }
+    public void setEmployee_id(String employee_id) {
+        this.employee_id = employee_id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword_hash() {
+        return password_hash;
+    }
+    public void setPassword_hash(String password_hash) {
+        this.password_hash = password_hash;
+    }
+}
