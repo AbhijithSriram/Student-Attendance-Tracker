ddeedbe97960fb591628dcaba36359c135f8621f
AbhijithSriram <abhijithsriramv28@gmail.com>
2025-10-21 05:04:00 +0530

    Complete Models

--- src/main/java/com/group1/model/Student.java
+++ src/main/java/com/group1/model/Student.java
@@ -3,6 +3,7 @@ package com.group1.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "STUDENT")
@@ -16,4 +17,17 @@ public class Student {
     public void setReg_number(String reg_number) { this.reg_number = reg_number; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(reg_number, student.reg_number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reg_number);
+    }
 }
